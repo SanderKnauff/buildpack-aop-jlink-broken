37f7a5cd4f4607ece758353aceabda6b71e98e29
Sander Knauff <S.knauff@alfen.com>
2024-10-17 17:01:51 +0200

    Minimal example of JLink breaking AOP when using buildpacks

--- src/main/java/ooo/sansk/demo/buildpacks/BuildpacksApplication.java
+++ src/main/java/ooo/sansk/demo/buildpacks/BuildpacksApplication.java
@@ -1,13 +1,36 @@
 package ooo.sansk.demo.buildpacks;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
 public class BuildpacksApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BuildpacksApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(BuildpacksApplication.class, args);
-	}
+    @Bean
+    CommandLineRunner runner(BasicRepository repository) {
+        return args -> {
+            System.out.println(repository);
+        };
+    }
 
+    @Bean
+    DBConfig dbConfig() {
+        return new DBConfig();
+    }
+
+    @Repository
+    class BasicRepository {
+        public BasicRepository(DBConfig dbConfig) {
+        }
+    }
 }
+
+record DBConfig() {}
+
